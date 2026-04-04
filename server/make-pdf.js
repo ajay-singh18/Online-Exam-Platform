5d9da88754fec1fbea783c3520d3705762262046
Ajay Kumar <ajaykrsingh8709@gmail.com>
2026-04-04 16:05:18 +0530

    chore: cleanup development test scripts

--- server/make-pdf.js
+++
@@ -1,7 +0,0 @@
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
-
-const doc = new PDFDocument();
-doc.pipe(fs.createWriteStream('sample.pdf'));
-doc.fontSize(25).text('Test Question 1: What is 2+2? A) 3 B) 4 Correct: B', 100, 100);
-doc.end();
--- server/parse-test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-async function test() {
-  const parseFunc = typeof pdf === 'function' ? pdf : (pdf.default || pdf.PDFParse);
-  console.log(typeof parseFunc);
-}
-test();
